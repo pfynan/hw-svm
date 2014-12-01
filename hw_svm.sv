e8886ce304f0ecbfdcfb78bca14179777fc1d8e3
linky <xianglin@andrew.cmu.edu>
2014-12-01 14:09:30 -0500

    tidied up hw_svm.sv

--- hw_svm.sv
+++ hw_svm.sv
@@ -8,15 +8,11 @@
   NOTE: "results" is valid ONE CYCLE AFTER "done" is asserted!
 */
 module hw_svm
-( input   logic clk, rst, start, //last_input,
-  //input   logic [`NUM_FEAT-1:0][`DATA_SIZE-1:0]               test_vector,
-  //input   logic [`NUM_SV-1:0][`NUM_FEAT-1:0][`DATA_SIZE-1:0]  support_vectors,
+( input   logic clk, rst, start,
   output  logic done);
-  //output  logic [`NUM_INST-1:0][`ACCUM_SIZE-1:0]  results);
 
   // support vectors
   logic [$clog2(`NUM_SV)-1:0]   sv_i;
-  //logic [`NUM_FEAT-1:0][`DATA_SIZE-1:0]                 sv_in;
   logic [`DATA_SIZE-1:0]  sv_in[`NUM_FEAT-1:0];
   logic [`DATA_SIZE-1:0]  support_vectors[`NUM_SV-1:0][`NUM_FEAT-1:0];
   // test vector stuff
@@ -114,11 +110,8 @@ endmodule: hw_svm
 module hw_svm_tb;
 
   // assume 3 SVs, 2 features, 2 test instances, 32 bit data, 64 bit accum
-  logic clk, rst, start;//, last_input;
-  //logic [`NUM_INST-1:0][`DATA_SIZE-1:0] test_vector;
-  //logic [2:0][1:0][31:0]  support_vectors;
+  logic clk, rst, start;
   logic done;
-  //logic [`NUM_INST-1:0][`ACCUM_SIZE-1:0]  results;
 
   int i;
 
@@ -130,27 +123,18 @@ module hw_svm_tb;
   end
 
   initial begin
-    rst = 1; start = 0; //last_input = 0; test_vector = 0; //support_vectors = 0;
+    rst = 1; start = 0;
     @(posedge clk);
     rst <= 0;
     @(posedge clk);
     // begin test sequence
-    //test_vector <= {32'd1, 32'd2};
-    //support_vectors <= {{32'd6, 32'd5}, {32'd4, 32'd3}, {32'd2, 32'd1}};
     start <= 1;
     @(posedge clk);
     start <= 0;
-    repeat (`NUM_SV-1) @(posedge clk);
-    //test_vector <= {32'd3, 32'd4};
-    //last_input <= 1;
-    @(posedge clk);
-    //last_input <= 0;
-    repeat (`NUM_SV-1) @(posedge clk);
     
     @(posedge done);
     repeat (2) @(posedge clk);
     for (i=0;i<`NUM_INST;i++) $display("%x", dut.results[i]);
-    //$writememh("results.hex", results);
     $finish;
   end
 
